348253598d3cacccc685f68a985543a1f0d0e6e8
Sa.Engineer <salma.ah67@gmail.com>
2026-09-22 11:41:20 +0300

    Add My To-Do List Console App

--- My class test/my-app/src/test/java/com/mycompany/app/TaskManagerTest.java
+++ My class test/my-app/src/test/java/com/mycompany/app/TaskManagerTest.java
@@ -6,33 +6,33 @@ import org.junit.Test;
 
 public class TaskManagerTest {
 
-    private TaskOperations manager1;
+    private TaskOperations manager;
 
     @Before
-    public void set_Up() {
-        manager1 = Task_Manager.getInstance();
-        manager1.getTasks().clear(); // Clear tasks before each test to ensure a clean state
+    public void setUp() {
+        manager = Task_Manager.getInstance();
+        manager.getTasks().clear(); // Clear tasks before each test to ensure a clean state
     }
 
-    // 1-assertEquals: Checking the title of the added task
+    // 1. assertEquals: Checking the title of the added task
     @Test
     public void testAddTaskTitle() {
-        manager1.addTask("Task 1");
-        assertEquals("Task 1", manager1.getTasks().get(0).getTaskTitle());
+        manager.addTask("Task 1");
+        assertEquals("Task 1", manager.getTasks().get(0).getTaskTitle());
     }
 
-    // 2-assertTrue: Checking the successful completion of an existing task
+    // 2. assertTrue: Checking the successful completion of an existing task
     @Test
     public void testCompleteTaskSuccess() {
-        manager1.addTask("Task 2");
-        boolean isCompleted = manager1.completeTask(0);
+        manager.addTask("Task 2");
+        boolean isCompleted = manager.completeTask(0);
         assertTrue(isCompleted);
     }
 
     // 3-assertFalse: Checking the failure to complete a task with a non-existent index
     @Test
     public void testCompleteTaskFailure() {
-        boolean isCompleted = manager1.completeTask(99);// Non-existent index
+        boolean isCompleted = manager.completeTask(99);// Non-existent index
         assertFalse(isCompleted);
     }
 
@@ -40,21 +40,21 @@ public class TaskManagerTest {
     @Test
     public void testSingletonInstance() {
         TaskOperations anotherInstance = Task_Manager.getInstance();
-        assertSame(manager1, anotherInstance);
+        assertSame(manager, anotherInstance);
     }
 
     // 5-assertNotNull: Checking that the tasks list is not null
     @Test
     public void testGetTasksNotNull() {
-        assertNotNull(manager1.getTasks());
+        assertNotNull(manager.getTasks());
     }
 
     // 6-assertNotEquals: Checking that the list size changes after adding an element
     @Test
     public void testListSizeChanges() {
-        int initialSize = manager1.getTasks().size(); // Store the initial size of the tasks list
-        manager1.addTask("Task 3");
-        assertNotEquals(initialSize, manager1.getTasks().size());
+        int initialSize = manager.getTasks().size(); // 0
+        manager.addTask("Task 3");
+        assertNotEquals(initialSize, manager.getTasks().size());
     }
 
 }
